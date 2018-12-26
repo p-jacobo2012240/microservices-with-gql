47bccdf8181afb69e704e4fdcfc9ce7f2c73ce7f
unknown <pablojacobo1998@gmail.com>
2018-12-25 18:09:23 -0600

    Implements View/Factura

--- src/main/java/com/hck/app/controllers/FacturaController.java
+++ src/main/java/com/hck/app/controllers/FacturaController.java
@@ -1,5 +1,43 @@
 package com.hck.app.controllers;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.hck.app.models.entity.Cliente;
+import com.hck.app.models.entity.Factura;
+import com.hck.app.models.service.IClienteService;
+
+@Controller
+@RequestMapping("/factura")
 public class FacturaController {
 
+	@Autowired
+	private IClienteService clienteService;
+	
+	@GetMapping("/form/{clienteId}")
+	public String crear(@PathVariable(value = "clienteId") Long clienteId, Map<String, Object> model,
+			RedirectAttributes flash) {
+
+		Cliente cliente = clienteService.findOne(clienteId);
+
+		if (cliente == null) {
+			flash.addFlashAttribute("error", "El cliente no existe en la base de datos");
+			return "redirect:/listar";
+		}
+
+		Factura factura = new Factura();
+		factura.setCliente(cliente);
+
+		model.put("factura", factura);
+		model.put("titulo", "Crear Factura");
+
+		return "factura/form";
+	}
+	
 }
