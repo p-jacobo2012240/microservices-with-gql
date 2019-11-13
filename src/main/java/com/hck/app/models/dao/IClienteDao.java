db482b53371c849f89c1c79e1fc1494a726b300a
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-11-12 22:35:57 -0600

    clean a project

--- src/main/java/com/hck/app/models/dao/IClienteDao.java
+++
@@ -1,9 +0,0 @@
-package com.hck.app.models.dao;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.hck.app.models.entity.Cliente;
-
-public interface IClienteDao extends PagingAndSortingRepository<Cliente, Long> {
-
-}
