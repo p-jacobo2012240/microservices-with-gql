ca0c1789a4557c18315ad9f40dc4b117128eb914
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-11-17 23:39:27 -0600

    backend crm-shop

---
+++ crm-back/src/main/java/org/hackobo/core/CrmBackApplication.java
@@ -0,0 +1,13 @@
+package org.hackobo.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmBackApplication.class, args);
+	}
+
+}
---
+++ crm-back/src/test/java/org/hackobo/core/CrmBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hackobo.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
