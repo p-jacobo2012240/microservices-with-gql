46f18c840ee41a4fdf66fbf9dc3d227d05518f24
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-03 16:28:38 -0600

    implements custom responses

---
+++ api-crm/src/main/java/org/hackobo/api/RestTemplateConfig.java
@@ -0,0 +1,15 @@
+package org.hackobo.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+	
+	@Bean
+	public RestTemplate registerRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
--- api-crm/src/main/java/org/hackobo/api/controllers/ProductController.java
+++ api-crm/src/main/java/org/hackobo/api/controllers/ProductController.java
@@ -4,21 +4,29 @@ import java.util.List;
 
 import org.hackobo.api.dao.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.hackobo.api.entity.Product;
 
 @RestController
+@RequestMapping("/api/v1")
 public class ProductController {
 	
 	@Autowired
 	private ProductRepository productDao;
 	
 	@GetMapping("/products")
-	public List<Product> findAllProducts(){
-		return (List<Product>) productDao.findAll();
+	public ResponseEntity<List<Product>> findAllProducts(){
+		List<Product> products  = (List<Product>) productDao.findAll();
+		if( products.isEmpty()) {
+			return new ResponseEntity<List<Product>>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<Product>>( products, HttpStatus.OK);
 	}
 	
 	@GetMapping("/product/{id}")
