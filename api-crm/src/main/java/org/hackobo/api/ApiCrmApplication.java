53b1fa4c0cb7e190e02164bd52b4e889a5545765
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-02 22:28:09 -0600

    whith errors

---
+++ api-crm/src/main/java/org/hackobo/api/ApiCrmApplication.java
@@ -0,0 +1,13 @@
+package org.hackobo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrmApplication.class, args);
+	}
+
+}
