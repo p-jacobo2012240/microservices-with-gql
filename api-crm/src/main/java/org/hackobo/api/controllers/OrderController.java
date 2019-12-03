63bbc7478820d820426ea69bd2a15d43f53ec862
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-03 15:45:24 -0600

    configure dataset and [Rest] -> controllers

--- api-crm/src/main/java/org/hackobo/api/controllers/OrderController.java
+++ api-crm/src/main/java/org/hackobo/api/controllers/OrderController.java
@@ -1,5 +1,31 @@
 package org.hackobo.api.controllers;
 
+import java.util.List;
+
+import org.hackobo.api.dao.OrderRepository;
+import org.hackobo.api.entity.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderController {
 
+	@Autowired
+	private OrderRepository orderDao;
+	
+	@GetMapping("/orders")
+	public List<Order> findAllOrders(){
+		return (List<Order>) this.orderDao.findAll();
+	}
+	
+	@GetMapping("/order/{id}")
+	public Order findByIdOrder(@PathVariable Long id ) {
+		return this.orderDao.findById(id).orElse(null);
+	}
+	
+	
+	
+	
 }
--- api-crm/src/main/java/org/hackobo/api/controllers/ProductController.java
+++ api-crm/src/main/java/org/hackobo/api/controllers/ProductController.java
@@ -1,5 +1,30 @@
 package org.hackobo.api.controllers;
 
+import java.util.List;
+
+import org.hackobo.api.dao.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.hackobo.api.entity.Product;
+
+@RestController
 public class ProductController {
+	
+	@Autowired
+	private ProductRepository productDao;
+	
+	@GetMapping("/products")
+	public List<Product> findAllProducts(){
+		return (List<Product>) productDao.findAll();
+	}
+	
+	@GetMapping("/product/{id}")
+	public Product findByIdProduct(@PathVariable Long id)  {
+		return this.productDao.findById(id).orElse(null);
+	}
+	
 
 }
