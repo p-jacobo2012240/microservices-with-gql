c2b64eca7d7dee1f6def4b894ece4eab2686d086
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-03 22:41:01 -0600

    handler refacotr

--- api-crm/src/main/java/org/hackobo/api/controllers/OrderController.java
+++ api-crm/src/main/java/org/hackobo/api/controllers/OrderController.java
@@ -1,10 +1,14 @@
 package org.hackobo.api.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.hackobo.api.dao.OrderRepository;
 import org.hackobo.api.entity.Order;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +18,15 @@ public class OrderController {
 
 	@Autowired
 	private OrderRepository orderDao;
+	private Map<String, Object> res = new HashMap();
 	
 	@GetMapping("/orders")
-	public List<Order> findAllOrders(){
-		return (List<Order>) this.orderDao.findAll();
+	public ResponseEntity<List<Order>> findAllOrders(){
+		List<Order> orders = (List<Order>) this.orderDao.findAll();
+		if( orders.isEmpty()) {
+			return new ResponseEntity<List<Order>>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<Order>>(orders, HttpStatus.OK);
 	}
 	
 	@GetMapping("/order/{id}")
