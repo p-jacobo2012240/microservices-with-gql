e1ad0335011e8d02f26644cd8a67037877b4a90c
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-04 23:22:11 -0600

    initial commit getwat crm

---
+++ api-crm-getway/src/main/java/org/hackobo/getway/ApiCrmGetwayApplication.java
@@ -0,0 +1,13 @@
+package org.hackobo.getway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrmGetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrmGetwayApplication.class, args);
+	}
+
+}
---
+++ api-crm-getway/src/test/java/org/hackobo/getway/ApiCrmGetwayApplicationTests.java
@@ -0,0 +1,16 @@
+package org.hackobo.getway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiCrmGetwayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
