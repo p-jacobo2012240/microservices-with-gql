15f859c931aeb0d80acaee4442de04af7b55a312
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-05 22:54:32 -0600

    api getway config

--- api-crm-getway/src/main/java/org/hackobo/getway/AppConfigGetway.java
+++ api-crm-getway/src/main/java/org/hackobo/getway/AppConfigGetway.java
@@ -1,8 +1,15 @@
 package org.hackobo.getway;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfigGetway {
-	//build method exec get data 
+	
+	@Bean
+	public RestTemplate registerRestTemplate() {
+		return new RestTemplate();
+	}
+	
 }
--- api-crm-getway/src/main/java/org/hackobo/getway/implementations/ProductServiceImpl.java
+++ api-crm-getway/src/main/java/org/hackobo/getway/implementations/ProductServiceImpl.java
@@ -1,5 +1,18 @@
 package org.hackobo.getway.implementations;
 
-public class ProductServiceImpl {
+import java.util.List;
+
+import org.hackobo.getway.entity.Product;
+import org.hackobo.getway.service.ProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+	@Override
+	public List<Product> listAllProduct() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
