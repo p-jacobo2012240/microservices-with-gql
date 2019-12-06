6fb23c28a2d309569efa9d00eae1ae981dd6fc8d
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-05 23:13:27 -0600

    [without] contructor

--- api-crm-getway/src/main/java/org/hackobo/getway/implementations/ProductServiceImpl.java
+++ api-crm-getway/src/main/java/org/hackobo/getway/implementations/ProductServiceImpl.java
@@ -1,18 +1,29 @@
 package org.hackobo.getway.implementations;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.assertj.core.util.Arrays;
 import org.hackobo.getway.entity.Product;
 import org.hackobo.getway.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
+	@Autowired
+	private RestTemplate restClient;
+	
 	@Override
 	public List<Product> listAllProduct() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Object> products = Arrays.asList( 
+			restClient.getForObject("http://localhost:8001/api/v1/products", Product[].class));
+		//use streams
+		return products.stream()
+				.map(p -> new Product( )) //verify constructors app
+				.collect(Collectors.toList()) ;
 	}
 
 }
