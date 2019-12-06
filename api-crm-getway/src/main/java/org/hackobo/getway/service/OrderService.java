2d731578e9938c72f9aed60a4c76ad6f564157d0
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-05 22:43:52 -0600

    [add] getter order and product

---
+++ api-crm-getway/src/main/java/org/hackobo/getway/service/OrderService.java
@@ -0,0 +1,5 @@
+package org.hackobo.getway.service;
+
+public interface OrderService {
+
+}
---
+++ api-crm-getway/src/main/java/org/hackobo/getway/service/ProductService.java
@@ -0,0 +1,12 @@
+package org.hackobo.getway.service;
+
+import java.util.List;
+
+import org.hackobo.getway.entity.Order;
+import org.hackobo.getway.entity.Product;
+
+public interface ProductService {
+	
+	public List<Product> listAllProduct();
+	
+}
