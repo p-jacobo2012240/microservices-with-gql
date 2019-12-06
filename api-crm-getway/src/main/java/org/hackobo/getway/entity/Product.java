eae5cd92aa067224d3b060b3c2ab6eae4c537186
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-05 22:30:28 -0600

    build a bean temporal

--- api-crm-getway/src/main/java/org/hackobo/getway/entity/Product.java
+++ api-crm-getway/src/main/java/org/hackobo/getway/entity/Product.java
@@ -1,5 +1,52 @@
 package org.hackobo.getway.entity;
 
+
 public class Product {
-	//build a bean
+
+	private Long idProduct;
+	private String name;
+	private String description;
+	private Double price;
+	
+	public Product() {}
+	
+	public Product(Long idProduct, String name, String description, Double price) {
+		super();
+		this.idProduct = idProduct;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+	
+	
+	public Long getIdProduct() {
+		return idProduct;
+	}
+	public void setIdProduct(Long idProduct) {
+		this.idProduct = idProduct;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	
+	
+	
+	
+	
 }
