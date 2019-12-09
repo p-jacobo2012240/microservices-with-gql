c05879044237128e2e62cdf78bbe0d1e2d334f10
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-08 23:06:35 -0600

    adding sql [dummy]

---
+++ api-crm/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO products(name, description, price) VALUES('cafe', 'its bettet in the world', 200);
+INSERT INTO products(name, description, price) VALUES('te', 'its bettet in the world', 100);
+INSERT INTO products(name, description, price) VALUES('azucar', 'its bettet in the world', 200);
+INSERT INTO products(name, description, price) VALUES('sal', 'its bettet in the world', 50);
