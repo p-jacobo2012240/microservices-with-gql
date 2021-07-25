f35871654f3a99c650086090ebcd7b69c9db15de
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-07-25 17:30:01 -0600

    open api

---
+++ open-api/hello-open-api/src/main/java/com/hackobo/HelloOpenApiApplication.java
@@ -0,0 +1,13 @@
+package com.hackobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloOpenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloOpenApiApplication.class, args);
+    }
+
+}
---
+++ open-api/hello-open-api/src/test/java/com/hackobo/HelloOpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackobo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloOpenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
